f8a8d53633324b707a0222e4d509cdd1c6d66793
utubo <utbutb@gmail.com>
2024-01-07 13:18:08 +0900

    🐞fix: Tabs will not be replaced. #1

--- src/background.js
+++ src/background.js
@@ -5,16 +5,8 @@ browser.tabs.onRemoved.addListener(async (_tabId, _removeInfo) => {
     const url = (await browser.storage.local.get('url'))?.url;
     if (!url) return;
     const current = (await browser.tabs.query({ active: true }))[0];
-    if (current && current.url && current.url !== 'about:blank') return;
-    try {
-      if (current) {
-        await browser.tabs.update({ url: url });
-        return;
-      }
-    } catch (e) {
-       console.log(e);
-    }
+    if (current) return;
     browser.tabs.create({ url: url });
-  }, 100);
+  }, 200);
 });
 
